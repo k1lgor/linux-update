16c123e6cf86d7618c5c33a6eabdd5b6743333a6
Plamen Ivanov <paco.iwanow@gmail.com>
2022-03-13 17:34:39 +0200

    update

--- linux_update.py
+++ linux_update.py
@@ -5,27 +5,28 @@ import os
 import sys
 
 
-def check_distro(distro):
+def check_distro(self, distro):
     print('Checking up your distro...')
+    self.distro = ''
     if os.system("cat /etc/os-release | grep -i arch &> /dev/null") == 0:
-        distro = 'arch'
+        self.distro = 'arch'
     elif os.system("cat /etc/os-release | egrep -iw 'debian|ubuntu|kali' &> /dev/null") == 0:
-        distro = 'debian'
+        self.distro = 'debian'
     elif os.system("cat /etc/os-release | grep -i rhel &> /dev/null") == 0:
-        distro = 'rhel'
+        self.distro = 'rhel'
     elif os.system("cat /etc/os-release | grep -i fedora &> /dev/null") == 0:
-        distro = 'fedora'
+        self.distro = 'fedora'
     elif os.system("cat /etc/os-release | grep -i suse &> /dev/null") == 0:
-        distro = 'suse'
+        self.distro = 'suse'
 
 
-def update_distro(distro):
+def update_distro(self):
     print('Updating your distro...')
-    if distro == 'arch':
+    if self.distro == 'arch':
         os.system("yes | pacman -Syyu")
         sleep(2)
         os.system("yes | pacman -Scc")
-    elif distro == 'debian':
+    elif self.distro == 'debian':
         os.system("app update && apt dist-upgrade -y")
         sleep(2)
         os.system("apt autoclean && apt autoremove -y")
@@ -33,26 +34,25 @@ def update_distro(distro):
         if os.system("dpkg -s aptitude &>/dev/null") != 0:
             os.system("apt install -y aptitude")
         os.system("apt aptitude purge ~c -y")
-    elif distro == 'rhel':
+    elif self.distro == 'rhel':
         os.system("yum check-update -y")
         sleep(2)
         os.system("yum clean all && yum update -y")
-    elif distro == 'suse':
+    elif self.distro == 'suse':
         os.system("zypper refresh && zypper update -y")
         sleep(2)
         os.system("zypper cc -a")
-    elif distro == 'fedora':
+    elif self.distro == 'fedora':
         os.system("dnf upgrade -y && dnf clean all")
     os.system("echo 3 > /proc/sys/vm/drop_caches")
 
 
-def clear_swap():
-    if os.system("blkid | grep swap &> /dev/null") == 0:
-        os.system("swapoff -a && swapon -a")
+# def clear_swap():
+#     if os.system("blkid | grep swap &> /dev/null") == 0:
+#         os.system("swapoff -a && swapon -a")
 
 
 if __name__ == '__main__':
-    distro = ''
     if os.geteuid() != 0:
         exit('''
             ERROR ==============
@@ -60,7 +60,6 @@ if __name__ == '__main__':
             ERROR ==============
             ''')
     else:
-        check_distro(distro)
-        update_distro(distro)
-        clear_swap()
-    exit("Linux updated!!!")
+        check_distro()
+        update_distro()
+        # clear_swap()
